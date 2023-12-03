48611029d49b89a57e58062aea5cc0a057e5c01c
leobuck <leobuck12@gmail.com>
2023-12-03 19:09:43 -0300

    Criando a entidade pedidos

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,20 @@ import { ProdutoModule } from './produto/produto.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresConfigService } from './config/postgres.config.service';
 import { ConfigModule } from '@nestjs/config';
+import { PedidoModule } from './pedido/pedido.module';
 
 @Module({
   imports: [
     UsuarioModule,
     ProdutoModule,
+    PedidoModule,
     ConfigModule.forRoot({
       isGlobal: true
     }),
     TypeOrmModule.forRootAsync({
       useClass: PostgresConfigService,
       inject: [PostgresConfigService]
-    })
+    }),
   ],
 })
 export class AppModule {}
