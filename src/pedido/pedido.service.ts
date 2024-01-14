b790a747539c357f84007c51a7b9947f9928ae59
leobuck <leobuck12@gmail.com>
2024-01-14 18:26:58 -0300

    Incluindo os produtos no pedido

--- src/pedido/pedido.service.ts
+++ src/pedido/pedido.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PedidoEntity } from './pedido.entity';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { UsuarioEntity } from '../usuario/usuario.entity';
 import { StatusPedido } from './enum/statuspedido.enum';
 import { CriaPedidoDto } from './dto/CriaPedidoDto.dto';
 import { ItemPedidoEntity } from './itempedido.entity';
+import { ProdutoEntity } from 'src/produto/produto.entity';
 
 @Injectable()
 export class PedidoService {
@@ -14,19 +15,27 @@ export class PedidoService {
     private readonly pedidoRepository: Repository<PedidoEntity>,
     @InjectRepository(UsuarioEntity)
     private readonly usuarioRepository: Repository<UsuarioEntity>,
+    @InjectRepository(ProdutoEntity)
+    private readonly produtoRepository: Repository<ProdutoEntity>,
   ) {}
 
   async cadastraPedido(usuarioId: string, dadosDoPedido: CriaPedidoDto) {
     const usuario = await this.usuarioRepository.findOneBy({ id: usuarioId });
+    const produtosIds = dadosDoPedido.itensPedido.map((itemPedido) => itemPedido.produtoId);
+    const produtosRelacionados = await this.produtoRepository.findBy({ id: In(produtosIds) });
 
     const pedidoEntity = new PedidoEntity();
     pedidoEntity.status = StatusPedido.EM_PROCESSAMENTO;
     pedidoEntity.usuario = usuario;
 
     const itensPedidoEntidades = dadosDoPedido.itensPedido.map((itemPedido) => {
+      const produtoRelacionado = produtosRelacionados.find((produto) => produto.id === itemPedido.produtoId);
+
       const itemPedidoEntity = new ItemPedidoEntity();
-      itemPedidoEntity.precoVenda = 10;
+      itemPedidoEntity.produto = produtoRelacionado;
+      itemPedidoEntity.precoVenda = produtoRelacionado.valor;
       itemPedidoEntity.quantidade = itemPedido.quantidade;
+      itemPedidoEntity.produto.quantidadeDisponivel -= itemPedido.quantidade;
       return itemPedidoEntity;
     });
 
