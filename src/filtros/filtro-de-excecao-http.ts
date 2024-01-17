556063d4b5d4ce193481575677d8d7588afff6fb
leobuck <leobuck12@gmail.com>
2024-01-16 21:51:02 -0300

    Capturando todos os erros

--- src/filtros/filtro-de-excecao-http.ts
+++ src/filtros/filtro-de-excecao-http.ts
@@ -3,19 +3,33 @@ import {
   Catch,
   ExceptionFilter,
   HttpException,
+  HttpStatus,
 } from '@nestjs/common';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 
-@Catch(HttpException)
+@Catch()
 export class FiltroDeExcecaoHttp implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: unknown, host: ArgumentsHost) {
     console.log(exception);
 
     const contexto = host.switchToHttp();
     const resposta = contexto.getResponse<Response>();
+    const requisicao = contexto.getRequest<Request>();
 
-    const status = exception.getStatus();
-    const body = exception.getResponse();
+    const { status, body } =
+      exception instanceof HttpException
+        ? {
+            status: exception.getStatus(),
+            body: exception.getResponse(),
+          }
+        : {
+            status: HttpStatus.INTERNAL_SERVER_ERROR,
+            body: {
+              statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+              timestamp: new Date().toISOString(),
+              path: requisicao.url,
+            },
+          };
 
     resposta.status(status).json(body);
   }
