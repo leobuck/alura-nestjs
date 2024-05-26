4319a61bd39f031e8acae81219806b10ebc86072
leobuck <leobuck12@gmail.com>
2024-05-26 11:39:53 -0300

    Logando erros para produção

--- src/recursos/filtros/filtro-de-excecao-global.ts
+++ src/recursos/filtros/filtro-de-excecao-global.ts
@@ -1,6 +1,7 @@
 import {
   ArgumentsHost,
   Catch,
+  ConsoleLogger,
   ExceptionFilter,
   HttpException,
   HttpStatus,
@@ -9,10 +10,14 @@ import { HttpAdapterHost } from '@nestjs/core';
 
 @Catch()
 export class FiltroDeExcecaoGlobal implements ExceptionFilter {
-  constructor(private adapterHost: HttpAdapterHost) {}
+  constructor(
+    private adapterHost: HttpAdapterHost,
+    private loggerNativo: ConsoleLogger,
+  ) {}
 
   catch(exception: unknown, host: ArgumentsHost) {
-    console.log(exception);
+    this.loggerNativo.error(exception);
+    console.error(exception);
 
     const { httpAdapter } = this.adapterHost;
 
