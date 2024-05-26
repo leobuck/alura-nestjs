22117fd67146959bb9b1d9286a01b53f3073348b
leobuck <leobuck12@gmail.com>
2024-05-26 10:45:06 -0300

    Obtendo o JWT do cabeçalho

--- src/modulos/autenticacao/autenticacao.guard.ts
+++ src/modulos/autenticacao/autenticacao.guard.ts
@@ -1,8 +1,24 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
 export class AutenticacaoGuard implements CanActivate {
-  canActivate(contexto: ExecutionContext): boolean {
-    return false;
+  async canActivate(contexto: ExecutionContext): Promise<boolean> {
+    const requisicao = contexto.switchToHttp().getRequest();
+    const token = this.extrairTokenDoCabecalho(requisicao);
+    if (!token) {
+      throw new UnauthorizedException('Erro de autenticação');
+    }
+    return true;
+  }
+
+  private extrairTokenDoCabecalho(requisicao: Request): string | undefined {
+    const [tipo, token] = requisicao.headers.authorization?.split(' ') ?? [];
+    return tipo === 'Bearer' ? token : undefined;
   }
 }
